6184e750730003e18cb1dd057c6823f5dcca0572
Adam Hott <24814301+CodingInGreen@users.noreply.github.com>
2024-03-10 23:52:51 +0000

    Modified 90 and -90 rotation every second

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,66 @@
 #![no_std]
 #![no_main]
 
-use esp32c3_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
 use esp_backtrace as _;
-use esp_println::println;
+use esp_hal::{
+    clock::ClockControl,
+    Delay,
+    gpio::IO,
+    ledc::{
+        channel::{self, ChannelIFace},
+        timer::{self, TimerIFace},
+        LSGlobalClkSource,
+        LowSpeed,
+        LEDC,
+    },
+    peripherals::Peripherals,
+    prelude::*,
+};
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
-
-    let clocks = ClockControl::max(system.clock_control).freeze();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
     let mut delay = Delay::new(&clocks);
 
-    println!("Hello world!");
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let led = io.pins.gpio0.into_push_pull_output();
+
+    let mut ledc = LEDC::new(peripherals.LEDC, &clocks);
+
+    ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
+
+    let mut lstimer0 = ledc.get_timer::<LowSpeed>(timer::Number::Timer0);
+
+    lstimer0
+        .configure(timer::config::Config {
+            duty: timer::config::Duty::Duty5Bit,
+            clock_source: timer::LSClockSource::APBClk,
+            frequency: 24u32.kHz(),
+        })
+        .unwrap();
+
+    let mut channel0 = ledc.get_channel(channel::Number::Channel0, led);
+    channel0
+        .configure(channel::config::Config {
+            timer: &lstimer0,
+            duty_pct: 10,
+            pin_config: channel::config::PinConfig::PushPull,
+        })
+        .unwrap();
+
+
+    let duty_neg90 = 2; // Example value for ~1ms pulse width, adjust based on calculation
+    let duty_90 = 4; // Example value for ~2ms pulse width, adjust based on calculation
+
     loop {
-        println!("Loop...");
-        delay.delay_ms(500u32);
+        // Pivot to "-90" degrees
+        channel0.set_duty(duty_neg90).unwrap();
+        delay.delay_ms(1000 as u32);
+
+        // Pivot to "90" degrees
+        channel0.set_duty(duty_90).unwrap();
+        delay.delay_ms(1000 as u32);
     }
 }
