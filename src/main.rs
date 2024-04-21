4ab9b767022986287fbb954af592b6e500391096
Adam Hott <hott@Adams-MacBook-Pro.local>
2024-04-21 12:38:28 +0100

    Add photo of setup

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use esp_hal::{
 
 #[entry]
 fn main() -> ! {
+    
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
@@ -28,12 +29,15 @@ fn main() -> ! {
     let gpio4 = io.pins.gpio4.into_push_pull_output();
     let gpio5 = io.pins.gpio5.into_push_pull_output();
 
+    
+
     let mut ledc = LEDC::new(peripherals.LEDC, &clocks);
 
     ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
 
     let mut lstimer0 = ledc.get_timer::<LowSpeed>(timer::Number::Timer0);
 
+     
     lstimer0
         .configure(timer::config::Config {
             duty: timer::config::Duty::Duty5Bit,
@@ -41,6 +45,7 @@ fn main() -> ! {
             frequency: 24u32.kHz(),
         })
         .unwrap();
+        
 
     let mut channel0: esp_hal::ledc::channel::Channel<'_, LowSpeed, GpioPin<Output<esp_hal::gpio::PushPull>, 0>> = ledc.get_channel(channel::Number::Channel0, gpio0);
     let mut channel1: esp_hal::ledc::channel::Channel<'_, LowSpeed, GpioPin<Output<esp_hal::gpio::PushPull>, 1>> = ledc.get_channel(channel::Number::Channel1, gpio1);
@@ -49,6 +54,7 @@ fn main() -> ! {
     let mut channel4: esp_hal::ledc::channel::Channel<'_, LowSpeed, GpioPin<Output<esp_hal::gpio::PushPull>, 4>> = ledc.get_channel(channel::Number::Channel4, gpio4);
     let mut channel5: esp_hal::ledc::channel::Channel<'_, LowSpeed, GpioPin<Output<esp_hal::gpio::PushPull>, 5>> = ledc.get_channel(channel::Number::Channel5, gpio5);
 
+     
     channel0
         .configure(channel::config::Config {
             timer: &lstimer0,
@@ -65,6 +71,7 @@ fn main() -> ! {
         })
         .unwrap();
 
+       
      channel2
         .configure(channel::config::Config {
             timer: &lstimer0,
@@ -72,6 +79,7 @@ fn main() -> ! {
             pin_config: channel::config::PinConfig::PushPull,
         })
         .unwrap();
+     
 
     channel3
         .configure(channel::config::Config {
@@ -101,6 +109,7 @@ fn main() -> ! {
     let duty_neg90 = 2; // Example value for ~1ms pulse width, adjust based on calculation
     let duty_90 = 4; // Example value for ~2ms pulse width, adjust based on calculation
 
+    
     loop {
         // Pivot to "-90" degrees
         channel0.set_duty(duty_neg90).unwrap();
@@ -117,6 +126,7 @@ fn main() -> ! {
         // Pivot to "90" degrees
         channel1.set_duty(duty_90).unwrap();
         delay.delay_ms(1000 as u32);
+        
         // Pivot to "-90" degrees
         channel2.set_duty(duty_neg90).unwrap();
         delay.delay_ms(1000 as u32);
@@ -132,6 +142,7 @@ fn main() -> ! {
         // Pivot to "90" degrees
         channel3.set_duty(duty_90).unwrap();
         delay.delay_ms(1000 as u32);
+        
         // Pivot to "-90" degrees
         channel4.set_duty(duty_neg90).unwrap();
         delay.delay_ms(1000 as u32);
